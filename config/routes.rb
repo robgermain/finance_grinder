99d6c1cfb63e0134d364f6792705c9c109e1c157
robgermain <robgermain@yahoo.com>
2019-08-01 22:59:25 -0400

    Removed explicit routes; added index to list.

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,4 @@ Rails.application.routes.draw do
   root to: 'payment_method_categories#index'
   resources :payment_method_categories
 
-  get      '/payment_method_categories'            => 'payment_method_categories#index'
-  get      '/payment_method_categories/new'        => 'payment_method_categories#new'
-  post     '/payment_method_categories'            => 'payment_method_categories#create'
-  get      '/payment_method_categories/:id'        => 'payment_method_categories#show'
-  get      '/payment_method_categories/:id/edit'   => 'payment_method_categories#edit'
-  patch    '/payment_method_categories/:id'        => 'payment_method_categories#update'
-  put    '/payment_method_categories/:id'        => 'payment_method_categories#update'
-  delete   '/payment_method_categories/:id'        => 'payment_method_categories#destroy'
-
 end
